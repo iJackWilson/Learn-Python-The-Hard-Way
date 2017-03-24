948aa42c0dfe14999b05fc33788ee6da445b943e
iJackWilson <jack.wilson8796@gmail.com>
2017-03-24 14:01:36 +0000

    Master
    Master

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Jack Wilson'
+my_age = 19 #not a lie
+my_height = 72 #inches
+my_weight = 75 #kilograms
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d kilograms heavy." %my_weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." %my_teeth
+
+#This line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
